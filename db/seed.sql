38fe9ae1e5add21510aa9dd5f34b5653be12b47f
Hakim Guidoum <gdmhkm05@gmail.com>
2019-10-12 14:28:29 -0400

    add seeds and user.js

--- db/seed.sql
+++ db/seed.sql
@@ -1,101 +1,101 @@
-USE project2_dev;
+;USE project2_dev;
+
+INSERT INTO Users
+    (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
+VALUES
+    ("Joe", "Gates", "jgates@gmail.com", "purple99", true, "cat", 2, "Brady", "2433 Folsom Lane", 27560, "Morrisville", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Joe", "Gates", "j@g.co", "purple99", true, "cat", 2, "Brady", "2433 Folsom Lane", 27560, "Morrisville", "NC");
+    ("Jane", "Jobs", "jjobs@gmail.com", "green99", false, "cat", 3, "Lolla", "700 Morreene Rd", 27705, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Jobs", "j@j.co", "green99", false, "cat", 3, "Lolla", "700 Morreene Rd", 27705, "Durham", "NC");
+    ("Hakim", "Guidoum", "hguidoum@gmail.com", "white99", false, "cat", 3, "Peach", "3100 Gateway Centre Blvd", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Hakim", "Guidoum", "j@j.co", "white99", false, "cat", 3, "Peach", "3100 Gateway Centre Blvd", 27560, " Morrisville ", "NC");
+    ("Dan", "Jobs", "djobs@gamil.com", "green99", false, "cat", 3, "Mimi", " 407 Gray Marble Rd ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Dan", "Jobs", "j@j.co", "green99", false, "cat", 3, "Mimi", " 407 Gray Marble Rd ", 27560, " Morrisville ", "NC");
+    ("Jane", "Doe", "jdoe@gamil.com", "green99", false, "cat", 3, "Tiger", " 132 Brentfield Loop", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Doe", "j@j.co", "green99", false, "cat", 3, "Tiger", " 132 Brentfield Loop", 27560, " Morrisville ", "NC");
+    ("Jill", "Jobs", "jill@gamil.com", "white99", false, "cat", 2, "Misty", " 406 Adelaide Cir ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jill", "Jobs", "j@j.co", "white99", false, "cat", 2, "Misty", " 406 Adelaide Cir ", 27560, " Morrisville ", "NC");
+    ("Janet", "Jobs", "janet@gmail.com", "green99", false, "dog", 3, "Max", " 1333 Merrion Park Ln ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Janet", "Jobs", "j@j.co", "green99", false, "dog", 3, "Max", " 1333 Merrion Park Ln ", 27560, " Morrisville ", "NC");
+    ("Bill", "Jobs", "bjobs@gamil.com", "summer99", false, "dog", 3, "Cooper", "118 Station Dr ", 27560, "Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Bill", "Jobs", "j@j.co", "summer99", false, "dog", 3, "Cooper", "118 Station Dr ", 27560, "Morrisville ", "NC");
+    ("Heather", "Jobs", "hjobs@gamil.com", "green99", false, "dog", 3, "Buddy", "205 Garden Square Ln ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Heather", "Jobs", "j@j.co", "green99", false, "dog", 3, "Buddy", "205 Garden Square Ln ", 27560, " Morrisville ", "NC");
+    ("Jeff", "Jobs", "jeff@gamil.com", "green99", false, "dog", 3, "Rocky", " 3224 Summitt Ridge Loop ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jeff", "Jobs", "j@j.co", "green99", false, "dog", 3, "Rocky", " 3224 Summitt Ridge Loop ", 27560, " Morrisville ", "NC");
+    ("Jane", "Fonda", "jfonda@gamil.com", "green99", false, "dog", 4, "Oliver", " 321 Commons Walk Cir ", 27560, " Morrisville ", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Fonda", "j@j.co", "green99", false, "dog", 4, "Oliver", " 321 Commons Walk Cir ", 27560, " Morrisville ", "NC");
+    ("Josh", "Jobs", "josh@gamil.com", "green99", false, "dog", 6, "Bear", " 1417 W Pettigrew St ", 27705, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Josh", "Jobs", "j@j.co", "green99", false, "dog", 6, "Bear", " 1417 W Pettigrew St ", 27705, "Durham", "NC");
+    ("Jane", "Jones", "jjones@gamil.com", "green99", false, "cat", 3, "Oscar", " 1114 Berkeley St ", 27705, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Jones", "j@j.co", "green99", false, "cat", 3, "Oscar", " 1114 Berkeley St ", 27705, "Durham", "NC");
+    ("Jane", "James", "jjames@gamil.com", "green99", false, "cat", 4, "Simba", " 7 Coolidge Pl ", 27705, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "James", "j@j.co", "green99", false, "cat", 4, "Simba", " 7 Coolidge Pl ", 27705, "Durham", "NC");
+    ("Jane", "Sims", "jsims@gamil.com", "green99", false, "cat", 5, "Coco", " 3 Morristown Cir ", 27705, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Sims", "j@j.co", "green99", false, "cat", 5, "Coco", " 3 Morristown Cir ", 27705, "Durham", "NC");
+    ("Pitt", "Jobs", "pjobs@gamil.com", "green99", false, "cat", 2, "Lucy", " 214 Sherron Rd ", 27703, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Pitt", "Jobs", "j@j.co", "green99", false, "cat", 2, "Lucy", " 214 Sherron Rd ", 27703, "Durham", "NC");
+    ("Jane", "Albert", "jalbert@gmail.com", "green99", false, "cat", 3, "Sacha", " 514 Bacon St ", 27703, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Jane", "Albert", "j@j.co", "green99", false, "cat", 3, "Sacha", " 514 Bacon St ", 27703, "Durham", "NC");
+    ("Paul", "Jobs", "pjobs@gmail.com", "green99", false, "cat", 6, "Sam", " 100 Braine Ct ", 27703, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Paul", "Jobs", "j@j.co", "green99", false, "cat", 6, "Sam", " 100 Braine Ct ", 27703, "Durham", "NC");
+    ("Julie", "Jobs", "julie@gmail.com", "green99", false, "dog", 3, "Bella", " 5111 Huxey Glenn Ct ", 27703, "Durham", "NC");
 
 INSERT INTO pets
     (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
 VALUES
-    ("Julie", "Jobs", "j@j.co", "green99", false, "dog", 3, "Bella", " 5111 Huxey Glenn Ct ", 27703, "Durham", "NC");
-
-INSERT INTO pets
-    (firstName, lastName, email, password, isAdmin, pet, petAge, petName, address, zipcode, city, state)
-VALUES
-    ("Jeff", "Albert", "j@j.co", "green99", false, "dat", 3, "Luna", " 6 Marsh Landing Ct ", 27703, "Durham", "NC");
+    ("Jeff", "Albert", "jalbert@gmail.com", "green99", false, "cat", 3, "Luna", " 6 Marsh Landing Ct ", 27703, "Durham", "NC");
