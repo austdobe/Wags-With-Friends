c800ae432d5ef4568072922c647fba872fd5d7e7
Austin <austin.dober@gmail.com>
2019-10-13 09:47:38 -0400

    fixed formatiing issues in code maps.js

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -1,29 +1,29 @@
-var displayMap = function(){
-    L.mapquest.key =process.env.mapquest_key;
+const displayMap = function () {
+    L.mapquest.key = process.env.mapquest_key;
 
     L.mapquest.geocoding().geocode(['27511', '27513', '27519', '27560', '27607'], createMap);
 
     // Creating map
-    function createMap(err, res) {
-        var map = L.mapquest.map('map', {
+    function createMap (err, res) {
+        const map = L.mapquest.map('map', {
         layers: L.mapquest.tileLayer('map'),
         center: [0, 0],
         zoom: 12
         }); 
-    var featureGroup = generateMarkersFeatureGroup(res);
-    // Add markers to the map and zoom to the features
-    featureGroup.addTo(map);
-    map.fitBounds(featureGroup.getBounds());
+        const featureGroup = generateMarkersFeatureGroup(res);
+        // Add markers to the map and zoom to the features
+        featureGroup.addTo(map);
+        map.fitBounds(featureGroup.getBounds());
     }
 
-    function generateMarkersFeatureGroup(res) {
-        var group = [];
-        for (var i = 0; i < res.results.length; i++) {
-            var location = res.results[i].locations[0];
-            var locationLatLng = location.latLng;
-            var marker = L.marker(locationLatLng, {icon: L.mapquest.icons.flag()})
+    function generateMarkersFeatureGroup (res) {
+        const group = [];
+        for (let i = 0; i < res.results.length; i++) {
+        const location = res.results[i].locations[0];
+        const locationLatLng = location.latLng;
+        const marker = L.marker(locationLatLng, { icon: L.mapquest.icons.flag() })
             .bindPopup(location.adminArea5 + ', ' + location.adminArea3);
-            group.push(marker);
+        group.push(marker);
         }
         return L.featureGroup(group);
     }
