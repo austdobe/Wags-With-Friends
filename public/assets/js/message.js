cb884c78d568e72a083c84c29b5bac4b6f29782c
Ethan <allric0922@gmail.com>
2019-10-18 13:23:04 -0400

    almost working messages

--- public/assets/js/message.js
+++ public/assets/js/message.js
@@ -1,36 +1,40 @@
-const messageAPI = {
-  saveMessage: function (message) {
-    return $.ajax({
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      type: 'POST',
-      url: 'api/messages',
-      data: JSON.stringify(message)
-    });
-  },
-  getMessage: function () {
-    return $.ajax({
-      url: 'api/messages',
-      type: 'GET'
-    });
-  },
-  deleteMessage: function (id) {
-    return $.ajax({
-      url: 'api/messages' + id,
-      type: 'DELETE'
-    });
-  }
-};
-
-$('submitMsg').on('click', function (event) {
-  event.preventDefault();
-
-  console.log(session.passport.user);
-  const message = {
-    message: $('messagePost').val().trim()
-    // userId:
+  const messageAPI = {
+    saveMessage: function (message) {
+      return $.ajax({
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        type: 'POST',
+        url: 'api/messages',
+        data: JSON.stringify(message)
+      });
+    },
+    getMessage: function () {
+      return $.ajax({
+        url: 'api/messages',
+        type: 'GET'
+      });
+    },
+    deleteMessage: function (id) {
+      return $.ajax({
+        url: 'api/messages' + id,
+        type: 'DELETE'
+      });
+    }
   };
 
-  messageAPI.saveMessage(message);
-});
+  const $submitBTN = $('#submit');
+
+  const handleMsgSubmit = function (event) {
+    console.log('works');
+    event.preventDefault();
+    const messageSent = {
+      message: $('#messagePost').val().trim(),
+      userId: $('#messagePost').data('reciver'),
+      senderId: $('#messagePost').data('sender')
+    };
+
+    messageAPI.saveMessage(messageSent);
+  };
+
+  $submitBTN.on('click', handleMsgSubmit);
